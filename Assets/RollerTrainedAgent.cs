1b191d66e0b4f4767c5aa68f9f8553c5f94a3a18
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2018-12-03 21:44:56 -0500

    feat: Add new scene to play against agent

---
+++ Assets/RollerTrainedAgent.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RollerTrainedAgent : Agent
+{
+    
+    public GameObject Target;
+    public float speed = 10;
+
+    Rigidbody rBody;
+    private TargetHandler targetHandler;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        targetHandler = Target.GetComponent<TargetHandler>();
+    }
+
+    public override void AgentReset()
+    {
+        if (this.transform.position.y < -1.0)
+        {
+            // the Agent fell
+            ResetPosition();
+        }
+        else
+        {
+            targetHandler.NewPosition();
+        }
+    }
+
+    private void ResetPosition()
+    {
+        this.transform.position = new Vector3(0, 0, 0);
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+    }
+
+    public override void CollectObservations()
+    {
+        Vector3 relativePosition = Target.transform.position - this.transform.position;
+
+        // relative position target VS ball
+        AddVectorObs(relativePosition.x / 5);
+        AddVectorObs(relativePosition.z / 5);
+
+        // distance to edge of platform 
+        AddVectorObs((this.transform.position.x + 5) / 5);
+        AddVectorObs((this.transform.position.x - 5) / 5);
+        AddVectorObs((this.transform.position.z + 5) / 5);
+        AddVectorObs((this.transform.position.z - 5) / 5);
+
+        // velocity
+        AddVectorObs(rBody.velocity.x / 5);
+        AddVectorObs(rBody.velocity.z / 5);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        // rewards
+        float distanceToTarget = Vector3.Distance(this.transform.position,
+                                                  Target.transform.position);
+
+        // reached target
+        if (distanceToTarget < 1.2f)
+        {
+            AddReward(2.0f);
+            targetHandler.NewPosition();
+        }
+
+        // time penalty
+        AddReward(-0.05f);
+
+        // distance penality
+        AddReward(distanceToTarget / 11.0f * -0.50f);
+
+        // fell off platform
+        if (this.transform.position.y < -1.0)
+        {
+            AddReward(-10.0f);
+            ResetPosition();
+        }
+
+        // actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * speed);
+    }
+}
---
+++ Assets/TargetHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NewPosition()
+    {
+        // move the target to a new spot
+        this.transform.position = new Vector3(Random.value * 8 - 4,
+                                      0.5f,
+                                      Random.value * 8 - 4 );
+    }
+}
