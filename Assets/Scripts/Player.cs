2550bb9c58f41774b8d236fef9269dc4f520ae2a
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2018-12-04 21:09:18 -0500

    feat: Added score

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10;
+    public GameObject target;
+
+    private Rigidbody rigidBody;
+    private TargetHandler targetHandler;
+    private Scorer scoreHandler;
+
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        targetHandler = target.GetComponent<TargetHandler>();
+        scoreHandler = FindObjectOfType<Scorer>();
+    }
+	
+	void Update ()
+    {
+        ProcessInput();
+        ProcessPosition();
+    }
+
+    private void ProcessPosition()
+    {
+        float distanceToTarget = Vector3.Distance(this.transform.position,
+                                                  target.transform.position);
+        // reached target
+        if (distanceToTarget < 1.2f)
+        {
+            scoreHandler.PlayerHit();
+            targetHandler.NewPosition();
+        }
+        // check if fell off
+        // TODO more robust
+        if (this.transform.position.y < -1.0)
+        {
+            scoreHandler.PlayerFell();
+            ResetPosition();
+        }
+    }
+
+    private void ResetPosition()
+    {
+        this.transform.position = new Vector3(0, 0, 0);
+        this.rigidBody.angularVelocity = Vector3.zero;
+        this.rigidBody.velocity = Vector3.zero;
+    }
+
+    private void ProcessInput()
+    {
+        float move_x = 0.0f;
+        float move_z = 0.0f;
+
+        if (Input.GetKey(KeyCode.UpArrow)){move_z = 1.0f;}
+        if (Input.GetKey(KeyCode.DownArrow)){move_z = -1.0f;}
+        if (Input.GetKey(KeyCode.RightArrow)) { move_x = 1.0f; }
+        if (Input.GetKey(KeyCode.LeftArrow)) { move_x = -1.0f; }
+
+        rigidBody.AddForce(new Vector3(move_x, 0, move_z) * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/RollerTrainedAgent.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RollerTrainedAgent : Agent
+{
+    
+    public GameObject Target;
+    public float speed = 10;
+
+    private Rigidbody _rigidBody;
+    private TargetHandler _targetHandler;
+    private Scorer _scoreHandler;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _targetHandler = Target.GetComponent<TargetHandler>();
+        _scoreHandler = FindObjectOfType<Scorer>();
+    }
+
+    public override void AgentReset()
+    {
+        if (this.transform.position.y < -1.0)
+        {
+            // the Agent fell
+            ResetPosition();
+        }
+        else
+        {
+            _targetHandler.NewPosition();
+        }
+    }
+
+    private void ResetPosition()
+    {
+        this.transform.position = new Vector3(0, 0, 0);
+        this._rigidBody.angularVelocity = Vector3.zero;
+        this._rigidBody.velocity = Vector3.zero;
+    }
+
+    public override void CollectObservations()
+    {
+        Vector3 relativePosition = Target.transform.position - this.transform.position;
+
+        // relative position target VS ball
+        AddVectorObs(relativePosition.x / 5);
+        AddVectorObs(relativePosition.z / 5);
+
+        // distance to edge of platform 
+        AddVectorObs((this.transform.position.x + 5) / 5);
+        AddVectorObs((this.transform.position.x - 5) / 5);
+        AddVectorObs((this.transform.position.z + 5) / 5);
+        AddVectorObs((this.transform.position.z - 5) / 5);
+
+        // velocity
+        AddVectorObs(_rigidBody.velocity.x / 5);
+        AddVectorObs(_rigidBody.velocity.z / 5);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        // rewards
+        float distanceToTarget = Vector3.Distance(this.transform.position,
+                                                  Target.transform.position);
+
+        // reached target
+        if (distanceToTarget < 1.2f)
+        {
+            AddReward(2.0f);
+            _scoreHandler.AgentHit();
+            _targetHandler.NewPosition();
+        }
+
+        // time penalty
+        AddReward(-0.05f);
+
+        // distance penality
+        AddReward(distanceToTarget / 11.0f * -0.50f);
+
+        // fell off platform
+        if (this.transform.position.y < -1.0)
+        {
+            AddReward(-10.0f);
+            _scoreHandler.AgentFell();
+            ResetPosition();
+        }
+
+        // actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        _rigidBody.AddForce(controlSignal * speed);
+    }
+}
